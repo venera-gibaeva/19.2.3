a8efe8f29258d8b1294d084101d43dab7fc330ff
venera-gibaeva <98085115+venera-gibaeva@users.noreply.github.com>
2022-04-13 09:55:23 +0300

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,9 @@
+class Calculator:
+    def multiply(self, x, y):
+        return x * y
+    def division(self, x, y):
+        return x / y
+    def addition(self, x, y):
+        return x + y
+    def subtraction(self, x, y):
+        return x - y
